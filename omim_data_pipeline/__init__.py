1d0eb9fcb12303b8104bf1dcdf3d01a0fc174c18
Joe Flack <joeflack4@gmail.com>
2021-08-17 17:42:29 -0400

    Initial commit

---
+++ omim_data_pipeline/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Init"""
+from omim_data_pipeline.omim_data_pipeline import get_codes_by_yyyy_mm
+from omim_data_pipeline.definitions.error import OmimDataPipelineError
